2801d38d5aab3f109428f53b7ca55d5ad4452b2d
glitch-afk <glitch-afk.proton.me>
2023-09-03 21:14:33 +0530

    fetcch final website

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        primary: "#2B67E8",
+        neutral_text: "#363C46",
+        border: "E6E7E9"
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+}
