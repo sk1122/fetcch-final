a0cbd1c6b726f52b189e0d6d4570f745d28b7625
Harsh <harshgajjar145@gmail.com>
2024-01-08 14:31:16 +0530

    fea: about-us page

---
+++ src/data/founders.ts
@@ -0,0 +1,22 @@
+export const FOUNDERS = [
+  {
+    name: "Mandar",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis est a ante semper, semper pharetra metus facilisis. Aenean ex ipsum, ullamcorper eget tempus quis, auctor in dolor.",
+    role: "CEO & Co-Founder",
+    image: "/assets/team/Mandar.jpg",
+    route: "/mandar",
+    email: "ray@fetcch.xyz",
+    twitter: "https://twitter.com/ray_mandar",
+    linkedin: "https://www.linkedin.com/in/mandar-fetcch"
+  },
+  {
+    name: "Satyam Kulkarni",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis est a ante semper, semper pharetra metus facilisis. Aenean ex ipsum, ullamcorper eget tempus quis, auctor in dolor.",
+    role: "CTO & Co-Founder",
+    image: "/assets/team/Satyam.png",
+    route: "/satyam",
+    email: "satyam@fetcch.xyz",
+    twitter: "https://x.com/_sk1122",
+    linkedin: "https://linkedin.com/in/satyam-kulkarni"
+  }
+]
