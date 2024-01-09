3dd5bcbeea050d6f9c0fad7718cfb2ca767584ed
Harsh <harshgajjar145@gmail.com>
2024-01-09 15:48:29 +0530

    fea: bio added

--- src/data/founders.ts
+++ src/data/founders.ts
@@ -1,22 +1,24 @@
 export const FOUNDERS = [
   {
     name: "Mandar",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis est a ante semper, semper pharetra metus facilisis. Aenean ex ipsum, ullamcorper eget tempus quis, auctor in dolor.",
+    bio: "Mandar Dange is the co-founder of Fetcch. At the helm of Fetcch, Mandar is instrumental in steering the company towards significant milestones.",
     role: "CEO & Co-Founder",
     image: "/assets/team/Mandar.jpg",
     route: "/mandar",
     email: "ray@fetcch.xyz",
     twitter: "https://twitter.com/ray_mandar",
-    linkedin: "https://www.linkedin.com/in/mandar-fetcch"
+    linkedin: "https://www.linkedin.com/in/mandar-fetcch",
+    telegram: "https://t.me/ray_fetcch"
   },
   {
     name: "Satyam Kulkarni",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis est a ante semper, semper pharetra metus facilisis. Aenean ex ipsum, ullamcorper eget tempus quis, auctor in dolor.",
+    bio: "Satyam Kulkarni is the CTO and co-founder of Fetcch. He has a deep passion for distributed systems, compilers, and virtual machines; with a particular focus on their best implementations in the blockchain.",
     role: "CTO & Co-Founder",
     image: "/assets/team/Satyam.png",
     route: "/satyam",
     email: "satyam@fetcch.xyz",
     twitter: "https://x.com/_sk1122",
-    linkedin: "https://linkedin.com/in/satyam-kulkarni"
+    linkedin: "https://linkedin.com/in/satyam-kulkarni",
+    telegram: "https://t.me/ray_fetcch"
   }
 ]
